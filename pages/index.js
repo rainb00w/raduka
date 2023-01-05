b3c9222eaf40b088eab4ca4a5ba0adbc41d5391d
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-01-05 21:30:50 +0200

    _v1n

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,10 @@ export default function Home() {
         <a href="https://www.youtube.com/@OleksandrRaduka">Youtube</a>
         <a href="https://www.instagram.com/o.raduka/">Instagram</a>
         <a href="https://t.me/o_raduka">Telegram</a>
-
+        <br></br>
         <a href="/recommended">Рекомендованная литература</a>
+        <a href="/the-way">Путь. Краткое изложение</a>
+        <a href="/the-book">Книга "Говорящий о солнце и луне"</a>
       </div>
     </div>
   );
