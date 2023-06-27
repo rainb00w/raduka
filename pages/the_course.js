dce959f4fe09de6d90a47dbefd8a1a6f9c4832a1
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-06-27 16:54:58 +0300

    _v1g

--- pages/the_course.js
+++ pages/the_course.js
@@ -22,7 +22,7 @@ export default function Recommended() {
       <div className={s.form_container}>
         <iframe
           width="540"
-          height="305"
+          height="340"
           src="https://758f5465.sibforms.com/serve/MUIFABqhqOaMYldORt8qk35fJCK4HjLvi1-u4sdSH9KtK5E-oFCFmI6DAafZ6OX08Jv4jyii8SfprhWq5z-Oz0ftkUD_Ba3wf5o74DBH3dqlViaXTL8oAYik1E_RFPoY5mZWaMCdK0AWnKanB-NiVYo3dnUnkr08Y7qKcBGw_4TFyruSDCFGfdIDhS37OMCS6snaNq-7GFHU1MsT"
           frameborder="0"
           scrolling="no"
