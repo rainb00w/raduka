250277602e523297c38a24748109834f53e46b01
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-06-27 16:28:36 +0300

    _v1f

--- pages/the_course.js
+++ pages/the_course.js
@@ -14,26 +14,27 @@ export default function Recommended() {
           height="100%"
           src="https://www.youtube.com/embed/VbWwsEWGuaQ"
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowfullscreen
         ></iframe>
       </div>
-
-      <iframe
-        width="540"
-        height="305"
-        src="https://758f5465.sibforms.com/serve/MUIFABqhqOaMYldORt8qk35fJCK4HjLvi1-u4sdSH9KtK5E-oFCFmI6DAafZ6OX08Jv4jyii8SfprhWq5z-Oz0ftkUD_Ba3wf5o74DBH3dqlViaXTL8oAYik1E_RFPoY5mZWaMCdK0AWnKanB-NiVYo3dnUnkr08Y7qKcBGw_4TFyruSDCFGfdIDhS37OMCS6snaNq-7GFHU1MsT"
-        frameborder="0"
-        scrolling="auto"
-        allowfullscreen
-        style={{
-          display: 'block',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          maxWidth: '100%',
-        }}
-      ></iframe>
+      <div className={s.form_container}>
+        <iframe
+          width="540"
+          height="305"
+          src="https://758f5465.sibforms.com/serve/MUIFABqhqOaMYldORt8qk35fJCK4HjLvi1-u4sdSH9KtK5E-oFCFmI6DAafZ6OX08Jv4jyii8SfprhWq5z-Oz0ftkUD_Ba3wf5o74DBH3dqlViaXTL8oAYik1E_RFPoY5mZWaMCdK0AWnKanB-NiVYo3dnUnkr08Y7qKcBGw_4TFyruSDCFGfdIDhS37OMCS6snaNq-7GFHU1MsT"
+          frameborder="0"
+          scrolling="no"
+          allowfullscreen
+          style={{
+            display: 'block',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            maxWidth: '100%',
+          }}
+        ></iframe>
+      </div>
 
       <a href="/" className={s.link}>
         Вернуться на главную
